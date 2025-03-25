4405307c2e0da2e1be8c62285eb2e770836448c5
bardiya13 <bardi.soltan@gmail.com>
2025-03-25 21:01:53 +0330

    test commit

--- MGFN/dataset.py
+++ MGFN/dataset.py
@@ -105,7 +105,7 @@ class Dataset(data.Dataset):
             name = self.list[index].split('/')[-1].strip('\n')[:-4]
 
         elif args.datasetname == 'SH':
-            features = np.load(self.feature_address, self.list[index].strip('\n'), allow_pickle=True)
+            features = np.load(os.path.join(self.feature_address, self.list[index].strip('\n')), allow_pickle=True)
             features = np.array(features, dtype=np.float32)
             name = self.list[index].split('/')[-1].strip('\n')[:-4]
 
