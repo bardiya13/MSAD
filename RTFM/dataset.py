eae3966b49afba7705f9e3010e190b4722b302f9
Bardia Soltan <bardisoltan@gmail.com>
2025-05-04 22:09:59 +0330

    test commit

--- RTFM/dataset.py
+++ RTFM/dataset.py
@@ -101,6 +101,10 @@ class Dataset(data.Dataset):
 
         # features = self.list[index].strip('\n').replace('/scratch/kf09/lz1278/MSAD-I3D-WS/', '/kaggle/input/')
 
+        else:
+            features=features
+
+
 
 
 
