9775a701db0cdc05b85b90e0ef78a92d391a22b7
Bardia Soltan <bardisoltan@gmail.com>
2025-05-04 22:07:10 +0330

    test commit

--- RTFM/dataset.py
+++ RTFM/dataset.py
@@ -101,9 +101,7 @@ class Dataset(data.Dataset):
 
         # features = self.list[index].strip('\n').replace('/scratch/kf09/lz1278/MSAD-I3D-WS/', '/kaggle/input/')
 
-        else:
-            features = np.load(
-                file_path.replace('/scratch/kf09/lz1278/MSAD-I3D-WS/', '/kaggle/input/msad-normal-test/'))
+
 
 
         features = np.array(features, dtype=np.float32)
