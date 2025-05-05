3cfe3504b82f3fd70dbb91f3e042c016387aa99e
Bardia Soltan <bardisoltan@gmail.com>
2025-05-05 21:23:31 +0330

    test commit

--- MGFN/dataset.py
+++ MGFN/dataset.py
@@ -1,9 +1,257 @@
-#######################
+# #######################
+# import torch.utils.data as data
+# import numpy as np
+# from utils.utils import process_feat
+# import torch
+# import os
+#
+# torch.set_default_tensor_type('torch.cuda.FloatTensor')
+# import option
+#
+# args = option.parse_args()
+#
+#
+# class Dataset(data.Dataset):
+#     def __init__(self, args, is_normal=True, transform=None, test_mode=False, is_preprocessed=False, shangatic=False):
+#
+#         self.modality = args.modality
+#         self.is_normal = is_normal
+#         self.shangatic = shangatic
+#         if shangatic:
+#             if test_mode:
+#                 self.feature_address = args.test_feature_address
+#                 self.label_dir = args.test_label_address
+#             else:
+#                 self.feature_address = args.train_feature_address
+#                 self.label_dir = args.train_label_address
+#
+#
+#
+#         if test_mode:
+#             self.rgb_list_file = args.test_rgb_list
+#         else:
+#             self.rgb_list_file = args.rgb_list
+#         self.tranform = transform
+#         self.test_mode = test_mode
+#         self._parse_list()
+#         self.num_frame = 0
+#         self.labels = None
+#         self.is_preprocessed = args.preprocessed
+#
+#     def _parse_list(self):
+#         self.list = list(open(self.rgb_list_file))
+#         self.list = [x.strip("\n").strip() for x in self.list]
+#         # self.frame_indices = [os.path.splitext(os.path.basename(item.strip()))[0] for item in self.list]
+#         if self.test_mode is False:
+#             if args.datasetname == 'UCF':
+#                 if self.is_normal:
+#                     self.list = self.list[810:]  # ucf 810; sht63; xd 9525
+#                     # print('normal list')
+#                     # print(self.list)
+#                 else:
+#                     self.list = self.list[:810]  # ucf 810; sht 63; 9525
+#                     # print('abnormal list')
+#                     # print(self.list)
+# #
+#             if args.datasetname == 'MSAD':
+#                 if self.is_normal:
+#                     self.list = self.list[120:]
+#                     # print('normal list')
+#                     # print(self.list)
+#                 else:
+#                     self.list = self.list[:120]
+#                     # print('abnormal list')
+#                     # print(self.list)
+#
+#             elif args.datasetname == 'XD':
+#                 if self.is_normal:
+#                     self.list = self.list[9525:]
+#                     # print('normal list')
+#                     # print(self.list)
+#                 else:
+#                     self.list = self.list[:9525]
+#                     # print('abnormal list')
+#                     # print(self.list)
+#
+#             elif args.datasetname == 'SH':
+#                 if self.is_normal:
+#                     self.list = [self.list[i] for i in range(len(self.list))
+#                                  if self.get_label(i) == 0]
+#
+#                 else:
+#                     self.list = [self.list[i] for i in range(len(self.list))
+#                                  if self.get_label(i) == 1]
+#
+#
+#
+#     def __getitem__(self, index):
+#         label = self.get_label(index)  # get video level label 0/1
+#         if args.datasetname == 'UCF':
+#             features = np.load(self.list[index].strip('\n'), allow_pickle=True)
+#             features = np.array(features, dtype=np.float32)
+#             name = self.list[index].split('/')[-1].strip('\n')[:-4]
+#         elif args.datasetname == 'XD':
+#             features = np.load(self.list[index].strip('\n'), allow_pickle=True)
+#             features = np.array(features, dtype=np.float32)
+#             name = self.list[index].split('/')[-1].strip('\n')[:-4]
+#
+#         elif args.datasetname == 'MSAD' or args.datasetname == 'CUHK':
+#             features = np.load(self.list[index].strip('\n'), allow_pickle=True)
+#             features = np.array(features, dtype=np.float32)
+#             name = self.list[index].split('/')[-1].strip('\n')[:-4]
+#
+#         elif args.datasetname == 'Ped2':
+#             features = np.load(self.list[index].strip('\n'), allow_pickle=True)
+#             features = np.array(features, dtype=np.float32)
+#             name = self.list[index].split('/')[-1].strip('\n')[:-4]
+#
+#         elif args.datasetname == 'SH':
+#
+#             features = np.load(os.path.join(self.feature_address, self.list[index].strip('\n')+".npy"), allow_pickle=True)
+#             features = np.array(features, dtype=np.float32)
+#             name = self.list[index].split('/')[-1].strip('\n')[:-4]
+#
+#         if self.tranform is not None:
+#             features = self.tranform(features)
+#         if self.test_mode:
+#             if args.datasetname == 'UCF':
+#                 # ------------ I3D --------------
+#                 # mag = np.linalg.norm(features, axis=2)[:,:, np.newaxis]
+#                 # features = np.concatenate((features,mag),axis = 2)
+#
+#                 # ------------ Swin ------------
+#                 mag = np.linalg.norm(features, axis=1)[:, np.newaxis]
+#                 features = np.concatenate((features, mag), axis=1)
+#
+#             elif args.datasetname == 'MSAD' or args.datasetname == 'CUHK':
+#                 # ------------ I3D --------------
+#                 mag = np.linalg.norm(features, axis=2)[:, :, np.newaxis]
+#                 features = np.concatenate((features, mag), axis=2)
+#
+#                 # ------------ Swin ------------
+#                 # mag = np.linalg.norm(features, axis=1)[:, np.newaxis]
+#                 # features = np.concatenate((features, mag), axis=1)
+#
+#             elif args.datasetname == 'XD':
+#                 mag = np.linalg.norm(features, axis=1)[:, np.newaxis]
+#                 features = np.concatenate((features, mag), axis=1)
+#             elif args.datasetname == 'Ped2':
+#                 mag = np.linalg.norm(features, axis=1)[:, np.newaxis]
+#                 features = np.concatenate((features, mag), axis=1)
+#             elif args.datasetname == 'SH':
+#                 mag = np.linalg.norm(features, axis=2)[:, :, np.newaxis]
+#                 features = np.concatenate((features, mag), axis=2)
+#             return features, name
+#         else:
+#             if args.datasetname == 'UCF':
+#                 if self.is_preprocessed:
+#                     return features, label
+#                 # ------------------------ for I3D ------------------------------
+#                 # features = features.transpose(1, 0, 2)  # [10, T, F]
+#                 # divided_features = []
+#
+#                 # divided_mag = []
+#                 # for feature in features:
+#                 #     feature = process_feat(feature, args.seg_length) #ucf(32,2048)
+#                 #     divided_features.append(feature)
+#                 #     divided_mag.append(np.linalg.norm(feature, axis=1)[:, np.newaxis])
+#                 # divided_features = np.array(divided_features, dtype=np.float32)
+#                 # divided_mag = np.array(divided_mag, dtype=np.float32)
+#                 # divided_features = np.concatenate((divided_features,divided_mag),axis = 2)
+#                 # return divided_features, label
+#
+#                 # ------------------------ for Swin --------------------------------
+#
+#                 feature = process_feat(features, 32)
+#                 # if args.add_mag_info == True:
+#                 feature_mag = np.linalg.norm(feature, axis=1)[:, np.newaxis]
+#                 feature = np.concatenate((feature, feature_mag), axis=1)
+#                 return feature, label
+#
+#             if args.datasetname == 'MSAD':
+#                 if self.is_preprocessed:
+#                     return features, label
+#                 # ------------------------ for I3D ------------------------------
+#                 features = features.transpose(1, 0, 2)  # [10, T, F]
+#                 divided_features = []
+#
+#                 divided_mag = []
+#                 for feature in features:
+#                     feature = process_feat(feature, args.seg_length)  # ucf(32,2048)
+#                     divided_features.append(feature)
+#                     divided_mag.append(np.linalg.norm(feature, axis=1)[:, np.newaxis])
+#                 divided_features = np.array(divided_features, dtype=np.float32)
+#                 divided_mag = np.array(divided_mag, dtype=np.float32)
+#                 divided_features = np.concatenate((divided_features, divided_mag), axis=2)
+#                 return divided_features, label
+#
+#                 # ------------------------ for Swin --------------------------------
+#
+#                 # feature = process_feat(features, 32)
+#                 # # if args.add_mag_info == True:
+#                 # feature_mag = np.linalg.norm(feature, axis=1)[:, np.newaxis]
+#                 # feature = np.concatenate((feature,feature_mag),axis = 1)
+#                 # return feature, label
+#
+#
+#
+#             elif args.datasetname == 'XD':
+#                 feature = process_feat(features, 32)
+#                 if args.add_mag_info == True:
+#                     feature_mag = np.linalg.norm(feature, axis=1)[:, np.newaxis]
+#                     feature = np.concatenate((feature, feature_mag), axis=1)
+#                 return feature, label
+#
+#             if args.datasetname == 'SH':
+#                 if self.is_preprocessed:
+#                     return features, label
+#                 features = features.transpose(1, 0, 2)  # [10, T, F]
+#                 divided_features = []
+#
+#                 divided_mag = []
+#                 for feature in features:
+#                     feature = process_feat(feature, args.seg_length)
+#                     divided_features.append(feature)
+#                     divided_mag.append(np.linalg.norm(feature, axis=1)[:, np.newaxis])
+#                 divided_features = np.array(divided_features, dtype=np.float32)
+#                 divided_mag = np.array(divided_mag, dtype=np.float32)
+#                 divided_features = np.concatenate((divided_features, divided_mag), axis=2)
+#                 return divided_features, label
+#
+#     def get_label(self, index):
+#         if self.shangatic:
+#             # Load the label file for the current frame
+#
+#             label_path = os.path.join(self.label_dir, f"{self.list[index]}_labels.npy")
+#
+#             # Load the numpy array
+#             label_array = np.load(label_path)
+#
+#             # Return 1 if sum > 0, else 0
+#             if np.sum(label_array) > 0:
+#                 return torch.tensor(1.0)
+#             else:
+#                 return torch.tensor(0.0)
+#         else:
+#             # Original logic for non-shangatic mode
+#             if self.is_normal:
+#                 # label[0] = 1
+#                 label = torch.tensor(0.0)
+#             else:
+#                 label = torch.tensor(1.0)
+#                 # label[1] = 1
+#             return label
+#
+#     def __len__(self):
+#         return len(self.list)
+#
+#     def get_num_frames(self):
+#         return self.num_frame
+# ##################################
 import torch.utils.data as data
 import numpy as np
 from utils.utils import process_feat
 import torch
-import os
 
 torch.set_default_tensor_type('torch.cuda.FloatTensor')
 import option
@@ -12,21 +260,9 @@ args = option.parse_args()
 
 
 class Dataset(data.Dataset):
-    def __init__(self, args, is_normal=True, transform=None, test_mode=False, is_preprocessed=False, shangatic=False):
-
+    def __init__(self, args, is_normal=True, transform=None, test_mode=False, is_preprocessed=False):
         self.modality = args.modality
         self.is_normal = is_normal
-        self.shangatic = shangatic
-        if shangatic:
-            if test_mode:
-                self.feature_address = args.test_feature_address
-                self.label_dir = args.test_label_address
-            else:
-                self.feature_address = args.train_feature_address
-                self.label_dir = args.train_label_address
-
-
-
         if test_mode:
             self.rgb_list_file = args.test_rgb_list
         else:
@@ -40,8 +276,6 @@ class Dataset(data.Dataset):
 
     def _parse_list(self):
         self.list = list(open(self.rgb_list_file))
-        self.list = [x.strip("\n").strip() for x in self.list]
-        # self.frame_indices = [os.path.splitext(os.path.basename(item.strip()))[0] for item in self.list]
         if self.test_mode is False:
             if args.datasetname == 'UCF':
                 if self.is_normal:
@@ -52,7 +286,7 @@ class Dataset(data.Dataset):
                     self.list = self.list[:810]  # ucf 810; sht 63; 9525
                     # print('abnormal list')
                     # print(self.list)
-#
+
             if args.datasetname == 'MSAD':
                 if self.is_normal:
                     self.list = self.list[120:]
@@ -75,14 +309,13 @@ class Dataset(data.Dataset):
 
             elif args.datasetname == 'SH':
                 if self.is_normal:
-                    self.list = [self.list[i] for i in range(len(self.list))
-                                 if self.get_label(i) == 0]
-
+                    self.list = self.list[63:]
+                    # print('normal list')
+                    # print(self.list)
                 else:
-                    self.list = [self.list[i] for i in range(len(self.list))
-                                 if self.get_label(i) == 1]
-
-
+                    self.list = self.list[:63]
+                    # print('abnormal list')
+                    # print(self.list)
 
     def __getitem__(self, index):
         label = self.get_label(index)  # get video level label 0/1
@@ -106,8 +339,7 @@ class Dataset(data.Dataset):
             name = self.list[index].split('/')[-1].strip('\n')[:-4]
 
         elif args.datasetname == 'SH':
-
-            features = np.load(os.path.join(self.feature_address, self.list[index].strip('\n')+".npy"), allow_pickle=True)
+            features = np.load(self.list[index].strip('\n'), allow_pickle=True)
             features = np.array(features, dtype=np.float32)
             name = self.list[index].split('/')[-1].strip('\n')[:-4]
 
@@ -219,32 +451,17 @@ class Dataset(data.Dataset):
                 return divided_features, label
 
     def get_label(self, index):
-        if self.shangatic:
-            # Load the label file for the current frame
-
-            label_path = os.path.join(self.label_dir, f"{self.list[index]}_labels.npy")
-
-            # Load the numpy array
-            label_array = np.load(label_path)
-
-            # Return 1 if sum > 0, else 0
-            if np.sum(label_array) > 0:
-                return torch.tensor(1.0)
-            else:
-                return torch.tensor(0.0)
+        if self.is_normal:
+            # label[0] = 1
+            label = torch.tensor(0.0)
         else:
-            # Original logic for non-shangatic mode
-            if self.is_normal:
-                # label[0] = 1
-                label = torch.tensor(0.0)
-            else:
-                label = torch.tensor(1.0)
-                # label[1] = 1
-            return label
+            label = torch.tensor(1.0)
+            # label[1] = 1
+        return label
 
     def __len__(self):
+
         return len(self.list)
 
     def get_num_frames(self):
         return self.num_frame
-##################################
--- MGFN/option.py
+++ MGFN/option.py
@@ -1,3 +1,56 @@
+# import argparse
+# import os
+#
+# def parse_args():
+#     parser = argparse.ArgumentParser(description='MGFN')
+#     parser.add_argument('--feat_extractor', default='i3d', choices=['i3d', 'c3d', 'swin'])
+#     parser.add_argument('--feature_size', type=int, default=2048, help='size of feature (default: UCF:2048//xd:1024)')
+#     parser.add_argument('--hiddensize', type=int, default=512, help='size of feature (default: 512)')
+#     parser.add_argument('--modality', default='RGB', help='the type of the input, RGB, AUDIO, or MIX')
+#     parser.add_argument('--rgb-list', default='/kaggle/working/ucsd-p2/file_names.txt', help='list of rgb features ')
+#     parser.add_argument('--test-rgb-list', default='/kaggle/working/ucsd-p2/file_names.txt', help='list of test rgb features')
+#     parser.add_argument('--gt', default="/kaggle/working/ground_truth_merged.npy", help='file of ground truth ')
+#     parser.add_argument('--mag_ratio', type=float, default=0.1, help='mag ratio')
+#     parser.add_argument('--comment', default='mgfn', help='comment for the ckpt name of the training')
+#
+#     parser.add_argument('--test_feature_address', type=str, default="/kaggle/working/ucsd-p2/featur", help='where did you store your test features?')
+#     parser.add_argument('--train_feature_address', type=str, default="/kaggle/input/kkkkkkkkk/feature_UCSD2/output", help='where did you store your train features?')
+#     parser.add_argument('--test_label_address', type=str, default="/kaggle/working/ucsd-p2/merged_labels", help='where did you store your test labels?')
+#     parser.add_argument('--train_label_address', type=str, default="/kaggle/input/ucsd-p2/train_labels_new/train_labels_new", help='where did you store your train labels?')
+#
+#
+#     parser.add_argument('--seg_length', type=int, default=32, help='default:32')
+#     parser.add_argument('--local_con', default='static', help='dynamic/static')
+#     #for dynamic
+#     parser.add_argument('--head_K', type=int, default=4, help='default = 4')
+#     #model structure
+#     parser.add_argument('--depths1', type=int, default=3, help='depths1')
+#     parser.add_argument('--depths2', type=int, default=3, help='depths2')
+#     parser.add_argument('--depths3', type=int, default=2, help='depths3')
+#
+#     parser.add_argument('--mgfn_type1', default='gb', help='mgfn_types1')
+#     parser.add_argument('--mgfn_type2', default='fb', help='mgfn_types2')
+#     parser.add_argument('--mgfn_type3', default='fb', help='mgfn_types3')
+#
+#     parser.add_argument('--dropout_rate', type=float, default=0.2, help='dropout rate')
+#     parser.add_argument('--gpus', type=str, default='0', help='gpus')
+#     parser.add_argument('--lr', type=str, default='[0.001]*15000', help='learning rates for steps(list form) default:0.001')
+#     parser.add_argument('--batch_size', type=int, default=16, help='number of instances in a batch of data (default: 16)')
+#     parser.add_argument('--workers', default=0, help='number of workers in dataloader')
+#     parser.add_argument('--model-name', default='mgfn', help='name to save model')
+#     parser.add_argument('--pretrained_ckpt', default= None, help='ckpt for pretrained model')
+#     parser.add_argument('--num-classes', type=int, default=2, help='number of class')
+#     parser.add_argument('--datasetname', default='MSAD', help='dataset to train on (default:UCF/XD/UCF-bg-fg-sepa )')  # !!!
+#     parser.add_argument('--preprocessed', action = 'store_true', help='if train set is already segmented')
+#     parser.add_argument('--plot-freq', type=int, default=10, help='frequency of plotting (default: 10)')
+#     parser.add_argument('--max-epoch', type=int, default=700, help='maximum iteration to train (default: 100)')
+#     parser.add_argument('--testing-model', default=None, help='The model used for testing.')
+#
+#     args = parser.parse_args()
+#     os.environ['CUDA_VISIBLE_DEVICES'] = args.gpus
+#     args.gpus = [i for i in range(len(args.gpus.split(',')))]
+#
+#     return args
 import argparse
 import os
 
@@ -7,16 +60,11 @@ def parse_args():
     parser.add_argument('--feature_size', type=int, default=2048, help='size of feature (default: UCF:2048//xd:1024)')
     parser.add_argument('--hiddensize', type=int, default=512, help='size of feature (default: 512)')
     parser.add_argument('--modality', default='RGB', help='the type of the input, RGB, AUDIO, or MIX')
-    parser.add_argument('--rgb-list', default='/kaggle/working/ucsd-p2/file_names.txt', help='list of rgb features ')
-    parser.add_argument('--test-rgb-list', default='/kaggle/working/ucsd-p2/file_names.txt', help='list of test rgb features')
-    parser.add_argument('--gt', default="/kaggle/working/ground_truth_merged.npy", help='file of ground truth ')
+    parser.add_argument('--rgb-list', default='./data/msad/msad-i3d.list', help='list of rgb features ')
+    parser.add_argument('--test-rgb-list', default='./data/msad/msad-i3d-test.list', help='list of test rgb features')
+    parser.add_argument('--gt', default='./data/msad/gt-MSAD-WS.npy', help='file of ground truth ')
     parser.add_argument('--mag_ratio', type=float, default=0.1, help='mag ratio')
     parser.add_argument('--comment', default='mgfn', help='comment for the ckpt name of the training')
-
-    parser.add_argument('--test_feature_address', type=str, default="/kaggle/working/ucsd-p2/featur", help='where did you store your test features?')
-    parser.add_argument('--train_feature_address', type=str, default="/kaggle/input/kkkkkkkkk/feature_UCSD2/output", help='where did you store your train features?')
-    parser.add_argument('--test_label_address', type=str, default="/kaggle/working/ucsd-p2/merged_labels", help='where did you store your test labels?')
-    parser.add_argument('--train_label_address', type=str, default="/kaggle/input/ucsd-p2/train_labels_new/train_labels_new", help='where did you store your train labels?')
 
 
     parser.add_argument('--seg_length', type=int, default=32, help='default:32')
@@ -43,7 +91,7 @@ def parse_args():
     parser.add_argument('--datasetname', default='MSAD', help='dataset to train on (default:UCF/XD/UCF-bg-fg-sepa )')  # !!!
     parser.add_argument('--preprocessed', action = 'store_true', help='if train set is already segmented')
     parser.add_argument('--plot-freq', type=int, default=10, help='frequency of plotting (default: 10)')
-    parser.add_argument('--max-epoch', type=int, default=700, help='maximum iteration to train (default: 100)')
+    parser.add_argument('--max-epoch', type=int, default=3000, help='maximum iteration to train (default: 100)')
     parser.add_argument('--testing-model', default=None, help='The model used for testing.')
 
     args = parser.parse_args()
