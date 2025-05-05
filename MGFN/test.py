e6c176b732c33c64da84c0851b745e4a5ca2958d
Bardia Soltan <bardisoltan@gmail.com>
2025-05-05 21:49:27 +0330

    test commit

--- MGFN/test.py
+++ MGFN/test.py
@@ -88,9 +88,9 @@ def test(dataloader, model, args, device):
             # print(inputs[0].shape)
             if len(input.size()) == 4:
                 input = input.permute(0, 2, 1, 3)
-                if i < 120:
+                if i < 122:
                     shape_sum += input.shape[2]
-                    if i == 119:
+                    if i ==119 :
                         print(f"Sum of first 120 input.shape[2] values: {shape_sum}")
             _, _, _, _, logits = model(input)
             logits = torch.squeeze(logits, 1)
