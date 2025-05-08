62d8766bbdfe63a5ad0988fe0b756c32042131ef
Bardia Soltan <bardisoltan@gmail.com>
2025-05-08 15:52:59 +0330

    test commit

--- RTFM/dataset.py
+++ RTFM/dataset.py
@@ -96,9 +96,9 @@ class Dataset(data.Dataset):
         label = self.get_label()  # get video level label 0/1
         file_path = (self.list[index].strip('\n'))+".npy"
         if self.test_mode is False:
-            file_path="/kaggle/input/tad-train-feauter/I3D_Feature_Extraction_resnet/output"+file_path
+            file_path="/kaggle/input/tad-train-feauter/I3D_Feature_Extraction_resnet/output/"+file_path
         else:
-            file_path = "/kaggle/input/tad-feauter-test-1/output_folder" + file_path
+            file_path = "/kaggle/input/tad-feauter-test-1/output_folder/" + file_path
         features = np.load(file_path, allow_pickle=True)
         features = np.array(features, dtype=np.float32)
         if self.tranform is not None:
